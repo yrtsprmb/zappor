3c5cec2ac8c2108e13e50c172d30a4863d966efb
manfred <manfred@dhcp26.cip.ifi.lmu.de>
2018-09-04 17:48:26 +0200

    test mit session und flash integriert

--- client/app.py
+++ client/app.py
@@ -1,5 +1,61 @@
 from flask import Flask, render_template, request
+
+#Formularzeugs
+from flask_wtf import FlaskForm
+from wtforms import (StringField,IntegerField,BooleanField,
+                     DateTimeField,RadioField,SelectField,
+                     TextField,TextAreaField,SubmitField)
+from wtforms.validators import DataRequired
+
+
+from flask import session,redirect,url_for
+from flask import flash # for flashing messages
+
+
 app = Flask(__name__)
+#todo: als umgebungsvariable verwenden
+app.config['SECRET_KEY'] = 'geheimnis'
+
+
+
+#todo: eigene validatoren verwenden
+#todo: Schieberegler anpassen/einbinden
+class PrivacyForm(FlaskForm):
+    p = IntegerField("Insert p value:", validators=[DataRequired()])
+    q = IntegerField("Insert q value:")
+    r = IntegerField("Insert r value:")
+    submit = SubmitField("Submit")
+
+### testarea
+
+class InfoForm(FlaskForm):
+    breed = StringField("What breed are you?", validators=[DataRequired()])
+    neutered = BooleanField("Have you been neutered?")
+    mood = RadioField('Please choose your mood:', choices=[('mood_one','Happy'),('mood_two','Excited')])
+    food_choice = SelectField(u'Pick your favorite food:', # das u steht fuer unicode, um fehler vorzubeugen
+                              choices=[('chi','Chicken'),('bf','Beef'),('fi','Fish')])
+    feedback = TextAreaField()
+    submit = SubmitField('Submit')
+
+
+@app.route('/test', methods=['GET','POST'])
+def test():
+    form = InfoForm()
+    if form.validate_on_submit():
+        session['breed'] = form.breed.data
+        session['neutered'] = form.neutered.data
+        session['mood'] = form.mood.data
+        session['food'] = form.food_choice.data
+        session['feedback'] = form.feedback.data
+        return redirect(url_for('thankyouinfoform'))
+
+    return render_template('test.html', form=form)
+
+@app.route('/thankyouinfoform')
+def thankyouinfoform():
+    return render_template('thankyoutest.html')
+
+### ende testarea
 
 @app.route('/')
 def index():
@@ -9,9 +65,16 @@ def index():
 def personal_page():
     return render_template('personal.html')
 
-@app.route('/privacy')
+@app.route('/privacy',methods=['GET','POST'])
 def privacy_page():
-    return render_template('privacy.html')
+    p = False
+    form = PrivacyForm()
+    if form.validate_on_submit():
+        flash('You just clicked the button')
+        p = form.p.data
+        form.p.data = ''
+        return redirect(url_for('privacy_page'))
+    return render_template('privacy.html',form=form,p=p)
 
 @app.route('/radio')
 def radio_page():
